5ac13efec205ec6e2b7fec99e39835247963bf74
farman ali <farman ali@LAPTOP-KDKHPNC8>
2019-07-28 01:00:56 +0530

    from farman

--- berp/src/berp/Main.java
+++ berp/src/berp/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
       System.out.println("Welcom fary");
       System.out.println("From Tutan");
+      System.out.println("from fary");
 	}
 
 }
